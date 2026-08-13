0a84b9fa50bae2f3d13c50a3514558af2671493b
Jay <Jayscat52322@icloud.com>
2026-08-13 07:43:07 -0700

    Initial Commit

---
+++ BlinkCast/BlinkCastApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlinkCastApp.swift
+//  BlinkCast
+//
+//  Created by Jay on 8/13/26.
+//
+
+import SwiftUI
+
+@main
+struct BlinkCastApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BlinkCast/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BlinkCast
+//
+//  Created by Jay on 8/13/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
